a0fd52dadb9e9477d737ccab4a0e7948bd537c4f
周宇明 <461310150@qq.com>
2018-04-29 14:00:55 +0800

    update vector

--- math/vector.hpp
+++ math/vector.hpp
@@ -5,7 +5,36 @@
 
 #include <type_traits>
 #include <stdexcept>
+#include <exception>
 #include <cmath>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <stdio.h>
+
+template<class T>
+class vec2;
+
+template<class T>
+class vec3;
+
+template<class T>
+class vec4;
+
+using vec2f = vec2<float>;
+using vec2d = vec2<double>;
+using vec2ld = vec2<long double>;
+
+using vec3f = vec3<float>;
+using vec3d = vec3<double>;
+using vec3ld = vec3<long double>;
+
+using vec4f = vec4<float>;
+using vec4d = vec4<double>;
+using vec4ld = vec4<long double>;
+
+#define FLOATING_POINT_EQUAL_THRESHOLD 0.000001
 
 template<class T>
 class vec2
@@ -18,30 +47,35 @@ public:
 public:
 	vec2()
 	{
-		static_assert(std::is_floating_point<T>::value, "T must be a floating-point type!");
+		static_assert(std::is_floating_point<T>::value, "Type T of vec2 must be a floating-point type!");
 		x = 0.0; y = 0.0;
 	}
 
 	explicit vec2(T value)
 	{
-		static_assert(std::is_floating_point<T>::value, "T must be a floating-point type!");
+		static_assert(std::is_floating_point<T>::value, "Type T of vec2 must be a floating-point type!");
 		x = value; y = value;
 	}
 
 	vec2(T x, T y)
 	{
-		static_assert(std::is_floating_point<T>::value, "T must be a floating-point type!");
+		static_assert(std::is_floating_point<T>::value, "Type T of vec2 must be a floating-point type!");
 		this->x = x; this->y = y;
 	}
 
 public:
-	T operator[](size_t index) const
+	T& operator[](size_t index)
 	{
 		if (index >= 2) { throw std::out_of_range("vec2 index out of range!"); }
 		return *(&x + index);
 	}
 
 public:
+	T* ptr()
+	{
+		return &x;
+	}
+
 	T length() const
 	{
 		return sqrt(x * x + y * y);
@@ -67,13 +101,27 @@ public:
 
 	bool is_normal() const
 	{
-		return this->length() == 1.0;
+		return equal(this->length(), static_cast<T>(1.0));
 	}
 
-	T dot(vec2<T> vec)
+	T dot(vec2<T> vec) const
 	{
 		return vec.x * this->x + vec.y * this->y;
 	}
+
+public:
+	std::string to_string() const
+	{
+		char buffer[256];
+		sprintf_s(buffer, 256, "vec2(%.2lf, %.2lf)", x, y);
+		return std::string(buffer);
+	}
+
+	template<class T>
+	friend std::ostream& operator<<(std::ostream& out, vec2<T>& vec);
+	
+	template<class T>
+	friend std::istream& operator>>(std::istream& in, vec2<T>& vec);
 };
 
 template<class T>
@@ -87,30 +135,35 @@ public:
 public:
 	vec3()
 	{
-		static_assert(std::is_floating_point<T>::value, "T must be a floating-point type!");
+		static_assert(std::is_floating_point<T>::value, "Type T of vec3 must be a floating-point type!");
 		x = 0.0; y = 0.0; z = 0.0;
 	}
 
 	explicit vec3(T value)
 	{
-		static_assert(std::is_floating_point<T>::value, "T must be a floating-point type!");
+		static_assert(std::is_floating_point<T>::value, "Type T of vec3 must be a floating-point type!");
 		x = value; y = value; z = value;
 	}
 
 	vec3(T x, T y, T z)
 	{
-		static_assert(std::is_floating_point<T>::value, "T must be a floating-point type!");
+		static_assert(std::is_floating_point<T>::value, "Type T of vec3 must be a floating-point type!");
 		this->x = x; this->y = y; this->z = z;
 	}
 
 public:
-	T operator[](size_t index) const
+	T& operator[](size_t index)
 	{
 		if (index >= 3) { throw std::out_of_range("vec3 index out of range!"); }
 		return *(&x + index);
 	}
 
 public:
+	T* ptr()
+	{
+		return &x;
+	}
+
 	T length() const
 	{
 		return sqrt(x * x + y * y + z * z);
@@ -137,18 +190,32 @@ public:
 
 	bool is_normal() const
 	{
-		return this->length() == 1.0;
+		return equal(this->length(), static_cast<T>(1.0));
 	}
 
-	T dot(vec3<T> vec)
+	T dot(vec3<T> vec) const
 	{
 		return vec.x * this->x + vec.y * this->y + vec.z * this->z;
 	}
 
-	vec3<T> cross(vec3<T> vec)
+	vec3<T> cross(vec3<T> vec) const
 	{
 		return vec3<T>(this->y * vec.z - this->z * vec.y, this->z * vec.x - this->x * vec.z, this->x * vec.y - this->y * vec.x);
 	}
+
+public:
+	std::string to_string() const
+	{
+		char buffer[256];
+		sprintf_s(buffer, 256, "vec3(%.2lf, %.2lf, %.2lf)", x, y, z);
+		return std::string(buffer);
+	}
+
+	template<class T>
+	friend std::ostream& operator<<(std::ostream& out, vec3<T>& vec);
+	
+	template<class T>
+	friend std::istream& operator>>(std::istream& in, vec3<T>& vec);
 };
 
 template<class T>
@@ -162,30 +229,35 @@ public:
 public:
 	vec4()
 	{
-		static_assert(std::is_floating_point<T>::value, "T must be a floating-point type!");
+		static_assert(std::is_floating_point<T>::value, "Type T of vec4 must be a floating-point type!");
 		x = 0.0; y = 0.0; z = 0.0; w = 0.0;
 	}
 
 	explicit vec4(T value)
 	{
-		static_assert(std::is_floating_point<T>::value, "T must be a floating-point type!");
+		static_assert(std::is_floating_point<T>::value, "Type T of vec4 must be a floating-point type!");
 		x = value; y = value; z = value; w = value;
 	}
 
 	vec4(T x, T y, T z, T w)
 	{
-		static_assert(std::is_floating_point<T>::value, "T must be a floating-point type!");
+		static_assert(std::is_floating_point<T>::value, "Type T of vec4 must be a floating-point type!");
 		this->x = x; this->y = y; this->z = z; this->w = w;
 	}
 
 public:
-	T operator[](size_t index) const
+	T& operator[](size_t index)
 	{
 		if (index >= 4) { throw std::out_of_range("vec4 index out of range!"); }
 		return *(&x + index);
 	}
 
 public:
+	T* ptr()
+	{
+		return &x;
+	}
+
 	T length() const
 	{
 		return sqrt(x * x + y * y + z * z + w * w);
@@ -213,13 +285,27 @@ public:
 
 	bool is_normal() const
 	{
-		return this->length() == 1.0;
+		return equal(this->length(), static_cast<T>(1.0));
 	}
 
-	T dot(vec4<T> vec)
+	T dot(vec4<T> vec) const
 	{
 		return vec.x * this->x + vec.y * this->y + vec.z * this->z + vec.w * this->w;
 	}
+
+public:
+	std::string to_string() const
+	{
+		char buffer[256];
+		sprintf_s(buffer, 256, "vec4(%.2lf, %.2lf, %.2lf, %.2lf)", x, y, z, w);
+		return std::string(buffer);
+	}
+
+	template<class T>
+	friend std::ostream& operator<<(std::ostream& out, vec4<T>& vec);
+	
+	template<class T>
+	friend std::istream& operator>>(std::istream& in, vec4<T>& vec);
 };
 
 template<class T>
@@ -339,19 +425,19 @@ inline T cross(vec3<T> vec1, vec3<T> vec2)
 template<class T>
 inline bool operator==(vec2<T> vec1, vec2<T> vec2)
 {
-	return vec1.x == vec2.x && vec1.y == vec2.y;
+	return equal(vec1.x, vec2.x) && equal(vec1.y, vec2.y);
 }
 
 template<class T>
 inline bool operator==(vec3<T> vec1, vec3<T> vec2)
 {
-	return vec1.x == vec2.x && vec1.y == vec2.y && vec1.z == vec2.z;
+	return equal(vec1.x, vec2.x) && equal(vec1.y, vec2.y) && equal(vec1.z, vec2.z);
 }
 
 template<class T>
 inline bool operator==(vec4<T> vec1, vec4<T> vec2)
 {
-	return vec1.x == vec2.x && vec1.y == vec2.y && vec1.z == vec2.z && vec1.w == vec2.w;
+	return equal(vec1.x, vec2.x) && equal(vec1.y, vec2.y) && equal(vec1.z, vec2.z) && equal(vec1.w, vec2.w);
 }
 
 template<class T>
@@ -373,6 +459,24 @@ inline bool operator!=(vec4<T> vec1, vec4<T> vec2)
 }
 
 template<class T>
+inline vec2<T> operator-(vec2<T> vec)
+{
+	return vec2<T>(-vec.x, -vec.y);
+}
+
+template<class T>
+inline vec3<T> operator-(vec3<T> vec)
+{
+	return vec3<T>(-vec.x, -vec.y, -vec.z);
+}
+
+template<class T>
+inline vec4<T> operator-(vec4<T> vec)
+{
+	return vec4<T>(-vec.x, -vec.y, -vec.z, -vec.w);
+}
+
+template<class T>
 inline vec2<T> operator+(vec2<T> vec1, vec2<T> vec2)
 {
 	return vec2<T>(vec1.x + vec2.x, vec1.y + vec2.y);
@@ -388,6 +492,42 @@ template<class T>
 inline vec4<T> operator+(vec4<T> vec1, vec4<T> vec2)
 {
 	return vec4<T>(vec1.x + vec2.x, vec1.y + vec2.y, vec1.z + vec2.z, vec1.w + vec2.w);
+}
+
+template<class T>
+inline vec2<T> operator+(vec2<T> vec, T t)
+{
+	return vec2<T>(vec.x + t, vec.y + t);
+}
+
+template<class T>
+inline vec3<T> operator+(vec3<T> vec, T t)
+{
+	return vec3<T>(vec.x + t, vec.y + t, vec.z + t);
+}
+
+template<class T>
+inline vec4<T> operator+(vec4<T> vec, T t)
+{
+	return vec4<T>(vec.x + t, vec.y + t, vec.z + t, vec.w + t);
+}
+
+template<class T>
+inline vec2<T> operator+(T t, vec2<T> vec)
+{
+	return vec2<T>(vec.x + t, vec.y + t);
+}
+
+template<class T>
+inline vec3<T> operator+(T t, vec3<T> vec)
+{
+	return vec3<T>(vec.x + t, vec.y + t, vec.z + t);
+}
+
+template<class T>
+inline vec4<T> operator+(T t, vec4<T> vec)
+{
+	return vec4<T>(vec.x + t, vec.y + t, vec.z + t, vec.w + t);
 }
 
 template<class T>
@@ -409,6 +549,42 @@ inline void operator+=(vec4<T>& vec1, vec4<T> vec2)
 }
 
 template<class T>
+inline vec3<T> operator+=(vec2<T>& vec, T t)
+{
+	vec.x += t; vec.y += t;
+}
+
+template<class T>
+inline vec3<T> operator+=(vec3<T>& vec, T t)
+{
+	vec.x += t; vec.y += t; vec.z += t;
+}
+
+template<class T>
+inline vec4<T> operator+=(vec4<T>& vec, T t)
+{
+	vec.x += t; vec.y += t; vec.z += t; vec.w += t;
+}
+
+template<class T>
+inline vec3<T> operator+=(T t, vec2<T>& vec)
+{
+	vec.x += t; vec.y += t;
+}
+
+template<class T>
+inline vec3<T> operator+=(T t, vec3<T>& vec)
+{
+	vec.x += t; vec.y += t; vec.z += t;
+}
+
+template<class T>
+inline vec4<T> operator+=(T t, vec4<T>& vec)
+{
+	vec.x += t; vec.y += t; vec.z += t; vec.w += t;
+}
+
+template<class T>
 inline vec2<T> operator-(vec2<T> vec1, vec2<T> vec2)
 {
 	return vec2<T>(vec1.x - vec2.x, vec1.y - vec2.y);
@@ -424,6 +600,42 @@ template<class T>
 inline vec4<T> operator-(vec4<T> vec1, vec4<T> vec2)
 {
 	return vec4<T>(vec1.x - vec2.x, vec1.y - vec2.y, vec1.z - vec2.z, vec1.w - vec2.w);
+}
+
+template<class T>
+inline vec2<T> operator-(vec2<T> vec, T t)
+{
+	return vec2<T>(vec.x - t, vec.y - t);
+}
+
+template<class T>
+inline vec3<T> operator-(vec3<T> vec, T t)
+{
+	return vec3<T>(vec.x - t, vec.y - t, vec.z - t);
+}
+
+template<class T>
+inline vec4<T> operator-(vec4<T> vec, T t)
+{
+	return vec4<T>(vec.x - t, vec.y - t, vec.z - t, vec.w - t);
+}
+
+template<class T>
+inline vec2<T> operator-(T t, vec2<T> vec)
+{
+	return vec2<T>(vec.x - t, vec.y - t);
+}
+
+template<class T>
+inline vec3<T> operator-(T t, vec3<T> vec)
+{
+	return vec3<T>(vec.x - t, vec.y - t, vec.z - t);
+}
+
+template<class T>
+inline vec4<T> operator-(T t, vec4<T> vec)
+{
+	return vec4<T>(vec.x - t, vec.y - t, vec.z - t, vec.w - t);
 }
 
 template<class T>
@@ -445,6 +657,42 @@ inline void operator-=(vec4<T>& vec1, vec4<T> vec2)
 }
 
 template<class T>
+inline vec3<T> operator-=(vec2<T>& vec, T t)
+{
+	vec.x -= t; vec.y -= t;
+}
+
+template<class T>
+inline vec3<T> operator-=(vec3<T>& vec, T t)
+{
+	vec.x -= t; vec.y -= t; vec.z -= t;
+}
+
+template<class T>
+inline vec4<T> operator-=(vec4<T>& vec, T t)
+{
+	vec.x -= t; vec.y -= t; vec.z -= t; vec.w -= t;
+}
+
+template<class T>
+inline vec3<T> operator-=(T t, vec2<T>& vec)
+{
+	vec.x -= t; vec.y -= t;
+}
+
+template<class T>
+inline vec3<T> operator-=(T t, vec3<T>& vec)
+{
+	vec.x -= t; vec.y -= t; vec.z -= t;
+}
+
+template<class T>
+inline vec4<T> operator-=(T t, vec4<T>& vec)
+{
+	vec.x -= t; vec.y -= t; vec.z -= t; vec.w -= t;
+}
+
+template<class T>
 inline vec2<T> operator*(vec2<T> vec1, vec2<T> vec2)
 {
 	return vec2<T>(vec1.x * vec2.x, vec1.y * vec2.y);
@@ -460,6 +708,42 @@ template<class T>
 inline vec4<T> operator*(vec4<T> vec1, vec4<T> vec2)
 {
 	return vec4<T>(vec1.x * vec2.x, vec1.y * vec2.y, vec1.z * vec2.z, vec1.w * vec2.w);
+}
+
+template<class T>
+inline vec2<T> operator*(vec2<T> vec, T t)
+{
+	return vec2<T>(vec.x * t, vec.y * t);
+}
+
+template<class T>
+inline vec3<T> operator*(vec3<T> vec, T t)
+{
+	return vec3<T>(vec.x * t, vec.y * t, vec.z * t);
+}
+
+template<class T>
+inline vec4<T> operator*(vec4<T> vec, T t)
+{
+	return vec4<T>(vec.x * t, vec.y * t, vec.z * t, vec.w * t);
+}
+
+template<class T>
+inline vec2<T> operator*(T t, vec2<T> vec)
+{
+	return vec2<T>(vec.x * t, vec.y * t);
+}
+
+template<class T>
+inline vec3<T> operator*(T t, vec3<T> vec)
+{
+	return vec3<T>(vec.x * t, vec.y * t, vec.z * t);
+}
+
+template<class T>
+inline vec4<T> operator*(T t, vec4<T> vec)
+{
+	return vec4<T>(vec.x * t, vec.y * t, vec.z * t, vec.w * t);
 }
 
 template<class T>
@@ -481,6 +765,42 @@ inline void operator*=(vec4<T>& vec1, vec4<T> vec2)
 }
 
 template<class T>
+inline vec3<T> operator*=(vec2<T>& vec, T t)
+{
+	vec.x *= t; vec.y *= t;
+}
+
+template<class T>
+inline vec3<T> operator*=(vec3<T>& vec, T t)
+{
+	vec.x *= t; vec.y *= t; vec.z *= t;
+}
+
+template<class T>
+inline vec4<T> operator*=(vec4<T>& vec, T t)
+{
+	vec.x *= t; vec.y *= t; vec.z *= t; vec.w *= t;
+}
+
+template<class T>
+inline vec3<T> operator*=(T t, vec2<T>& vec)
+{
+	vec.x *= t; vec.y *= t;
+}
+
+template<class T>
+inline vec3<T> operator*=(T t, vec3<T>& vec)
+{
+	vec.x *= t; vec.y *= t; vec.z *= t;
+}
+
+template<class T>
+inline vec4<T> operator*=(T t, vec4<T>& vec)
+{
+	vec.x *= t; vec.y *= t; vec.z *= t; vec.w *= t;
+}
+
+template<class T>
 inline vec2<T> operator/(vec2<T> vec1, vec2<T> vec2)
 {
 	return vec2<T>(vec1.x / vec2.x, vec1.y / vec2.y);
@@ -499,6 +819,42 @@ inline vec4<T> operator/(vec4<T> vec1, vec4<T> vec2)
 }
 
 template<class T>
+inline vec2<T> operator/(vec2<T> vec, T t)
+{
+	return vec2<T>(vec.x / t, vec.y / t);
+}
+
+template<class T>
+inline vec3<T> operator/(vec3<T> vec, T t)
+{
+	return vec3<T>(vec.x / t, vec.y / t, vec.z / t);
+}
+
+template<class T>
+inline vec4<T> operator/(vec4<T> vec, T t)
+{
+	return vec4<T>(vec.x / t, vec.y / t, vec.z / t, vec.w / t);
+}
+
+template<class T>
+inline vec2<T> operator/(T t, vec2<T> vec)
+{
+	return vec2<T>(vec.x / t, vec.y / t);
+}
+
+template<class T>
+inline vec3<T> operator/(T t, vec3<T> vec)
+{
+	return vec3<T>(vec.x / t, vec.y / t, vec.z / t);
+}
+
+template<class T>
+inline vec4<T> operator/(T t, vec4<T> vec)
+{
+	return vec4<T>(vec.x / t, vec.y / t, vec.z / t, vec.w / t);
+}
+
+template<class T>
 inline void operator/=(vec2<T>& vec1, vec2<T> vec2)
 {
 	vec1.x /= vec2.x; vec1.y /= vec2.y;
@@ -514,6 +870,42 @@ template<class T>
 inline void operator/=(vec4<T>& vec1, vec4<T> vec2)
 {
 	vec1.x /= vec2.x; vec1.y /= vec2.y; vec1.z /= vec2.z; vec1.w /= vec2.w;
+}
+
+template<class T>
+inline vec3<T> operator/=(vec2<T>& vec, T t)
+{
+	vec.x /= t; vec.y /= t;
+}
+
+template<class T>
+inline vec3<T> operator/=(vec3<T>& vec, T t)
+{
+	vec.x /= t; vec.y /= t; vec.z /= t;
+}
+
+template<class T>
+inline vec4<T> operator/=(vec4<T>& vec, T t)
+{
+	vec.x /= t; vec.y /= t; vec.z /= t; vec.w /= t;
+}
+
+template<class T>
+inline vec3<T> operator/=(T t, vec2<T>& vec)
+{
+	vec.x /= t; vec.y /= t;
+}
+
+template<class T>
+inline vec3<T> operator/=(T t, vec3<T>& vec)
+{
+	vec.x /= t; vec.y /= t; vec.z /= t;
+}
+
+template<class T>
+inline vec4<T> operator/=(T t, vec4<T>& vec)
+{
+	vec.x /= t; vec.y /= t; vec.z /= t; vec.w /= t;
 }
 
 template<class T>
@@ -779,6 +1171,148 @@ template<class T>
 inline vec4<T> smooth_interpolation(vec4<T> vec1, vec4<T> vec2, T t)
 {
 	return vec4<T>(smooth_interpolation(vec1.x, vec2.x, t), smooth_interpolation(vec1.y, vec2.y, t), smooth_interpolation(vec1.z, vec2.z, t), smooth_interpolation(vec1.w, vec2.w, t));
+}
+
+template<class T>
+inline bool equal(T t1, T t2, T threshold = FLOATING_POINT_EQUAL_THRESHOLD)
+{
+	static_assert(std::is_floating_point<T>::value, "Type T of function equal(T t1, T t2, T threshold = 0.0001) must be a floating-point type!");
+	return abs(t1 - t2) <= threshold;
+}
+
+template<class T>
+inline std::string to_string(vec2<T> vec)
+{
+	return vec.to_string();
+}
+
+template<class T>
+inline std::string to_string(vec3<T> vec)
+{
+	return vec.to_string();
+}
+
+template<class T>
+inline std::string to_string(vec4<T> vec)
+{
+	return vec.to_string();
+}
+
+inline vec2<float> parse_vec2f(std::string text)
+{
+	std::stringstream stream(text);
+	vec2<float> vec;
+	stream >> vec.x >> vec.y;
+	return vec;
+}
+
+inline vec3<float> parse_vec3f(std::string text)
+{
+	std::stringstream stream(text);
+	vec3<float> vec;
+	stream >> vec.x >> vec.y >> vec.z;
+	return vec;
+}
+
+inline vec4<float> parse_vec4f(std::string text)
+{
+	std::stringstream stream(text);
+	vec4<float> vec;
+	stream >> vec.x >> vec.y >> vec.z >> vec.w;
+	return vec;
+}
+
+inline vec2<double> parse_vec2d(std::string text)
+{
+	std::stringstream stream(text);
+	vec2<double> vec;
+	stream >> vec.x >> vec.y;
+	return vec;
+}
+
+inline vec3<double> parse_vec3d(std::string text)
+{
+	std::stringstream stream(text);
+	vec3<double> vec;
+	stream >> vec.x >> vec.y >> vec.z;
+	return vec;
+}
+
+inline vec4<double> parse_vec4d(std::string text)
+{
+	std::stringstream stream(text);
+	vec4<double> vec;
+	stream >> vec.x >> vec.y >> vec.z >> vec.w;
+	return vec;
+}
+
+inline vec2<long double> parse_vec2ld(std::string text)
+{
+	std::stringstream stream(text);
+	vec2<long double> vec;
+	stream >> vec.x >> vec.y;
+	return vec;
+}
+
+inline vec3<long double> parse_vec3ld(std::string text)
+{
+	std::stringstream stream(text);
+	vec3<long double> vec;
+	stream >> vec.x >> vec.y >> vec.z;
+	return vec;
+}
+
+inline vec4<long double> parse_vec4ld(std::string text)
+{
+	std::stringstream stream(text);
+	vec4<long double> vec;
+	stream >> vec.x >> vec.y >> vec.z >> vec.w;
+	return vec;
+}
+
+template<class T>
+inline std::ostream& operator<<(std::ostream& out, vec2<T>& vec)
+{
+	out.setf(std::ios::fixed);
+	out << std::ios::fixed << std::setprecision(2) << "vec4(" << vec.x << ", " << vec.y << ")";
+	return out;
+}
+
+template<class T>
+inline std::istream& operator>>(std::istream& in, vec2<T>& vec)
+{
+	in >> vec.x >> vec.y;
+	return in;
+}
+
+template<class T>
+inline std::ostream& operator<<(std::ostream& out, vec3<T>& vec)
+{
+	out.setf(std::ios::fixed);
+	out << std::ios::fixed << std::setprecision(2) << "vec4(" << vec.x << ", " << vec.y << ", " << vec.z << ")";
+	return out;
+}
+
+template<class T>
+inline std::istream& operator>>(std::istream& in, vec3<T>& vec)
+{
+	in >> vec.x >> vec.y >> vec.z;
+	return in;
+}
+
+template<class T>
+inline std::ostream& operator<<(std::ostream& out, vec4<T>& vec)
+{
+	out.setf(std::ios::fixed);
+	out << std::ios::fixed << std::setprecision(2) << "vec4(" << vec.x << ", " << vec.y << ", " << vec.z << ", " << vec.w << ")";
+	return out;
+}
+
+template<class T>
+inline std::istream& operator>>(std::istream& in, vec4<T>& vec)
+{
+	in >> vec.x >> vec.y >> vec.z >> vec.w;
+	return in;
 }
 
 #endif // !__VECTOR__
